936d49ef1cb6dd1faf13aeb164ea327e5769c5f1
Kyle Morris <exxxzotiq@gmail.com>
2019-08-01 02:01:53 -0600

    - input_handlers takes care of our player movement outside of our main 'engine.py' file - arial10x10.png is a file we read from for games ASCII

---
+++ input_handlers.py
@@ -0,0 +1,25 @@
+import tcod as libtcod
+
+
+def handle_keys(key):
+    #movement keys, all return dict values that will be
+    #stored in 'action' var in engine.py
+    if key.vk == libtcod.KEY_UP:
+        return {'move': (0,-1)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {'move':(0,1)}
+    elif key.vk == libtcod.KEY_LEFT:
+        return {'move':(-1,0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {'move':(1,0)}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        #alt+enter: toggle full screen
+        return {'fullscreen': True}
+    
+    elif key.vk == libtcod.KEY_ESCAPE:
+        #exit game
+        return {'exit': True}
+
+    #no key pressed
+    return{}
